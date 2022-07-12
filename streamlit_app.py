d0e524f14076ba14c80fe2a10e9e57ec0309152d
eddyAtUrbern <63434918+eddyAtUrbern@users.noreply.github.com>
2022-07-12 11:26:07 +1000

    fixed "

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,7 +77,7 @@ def output(df, account_names, file_name):
 st.title('Uber pickups in NYC')
 
 st.file_uploader('Upload a CSV')
-uploaded_file = st.file_uploader(“Choose a file”)
+uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
 #read csv
 df=pd.read_csv(uploaded_file)
